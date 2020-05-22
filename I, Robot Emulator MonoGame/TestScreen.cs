15f8aef925b740ad06bff55ea01ef82cde76b939
Unknown <jmanfreda@lci1.com>
2020-05-22 09:22:59 -0400

    debugging / test code

--- I, Robot Emulator MonoGame/TestScreen.cs
+++ I, Robot Emulator MonoGame/TestScreen.cs
@@ -1,6 +1,7 @@
 ﻿using GameManagement;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -23,12 +24,125 @@ namespace I_Robot
         VertexPositionColor[] triangleVertices;
         VertexBuffer vertexBuffer;
 
-        //Orbit
         bool orbit = false;
 
         public TestScreen(ScreenManager screenManager)
             : base(screenManager)
         {
+
+
+            //Setup Camera
+            camTarget = new Vector3(0f, 0f, 0f);
+            camPosition = new Vector3(0f, 0f, -100f);
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+                               MathHelper.ToRadians(45f),
+                               GraphicsDevice.DisplayMode.AspectRatio,
+                1f, 1000f);
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,
+                         new Vector3(0f, 1f, 0f));// Y up
+            worldMatrix = Matrix.CreateWorld(camTarget, Vector3.
+                          Forward, Vector3.Up);
+
+            //BasicEffect
+            basicEffect = new BasicEffect(GraphicsDevice);
+            basicEffect.Alpha = 1f;
+
+            // Want to see the colors of the vertices, this needs to be on
+            basicEffect.VertexColorEnabled = true;
+
+            // Lighting requires normal information which VertexPositionColor does not have
+            // If you want to use lighting and VPC you need to create a custom def
+            basicEffect.LightingEnabled = false;
+
+            //Geometry  - a simple triangle about the origin
+            triangleVertices = new VertexPositionColor[3];
+            triangleVertices[0] = new VertexPositionColor(new Vector3(
+                                  0, 20, 0), Color.Red);
+            triangleVertices[1] = new VertexPositionColor(new Vector3(-
+                                  20, -20, 0), Color.Green);
+            triangleVertices[2] = new VertexPositionColor(new Vector3(
+                                  20, -20, 0), Color.Blue);
+
+            //Vert buffer
+            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(
+                           VertexPositionColor), 3, BufferUsage.
+                           WriteOnly);
+            vertexBuffer.SetData<VertexPositionColor>(triangleVertices);
+        }
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back ==
+                        ButtonState.Pressed || Keyboard.GetState().IsKeyDown(
+                        Keys.Escape))
+                Game.Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                camPosition.X -= 1f;
+                camTarget.X -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                camPosition.X += 1f;
+                camTarget.X += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                camPosition.Y -= 1f;
+                camTarget.Y -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                camPosition.Y += 1f;
+                camTarget.Y += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemPlus))
+            {
+                camPosition.Z += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemMinus))
+            {
+                camPosition.Z -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                orbit = !orbit;
+            }
+
+            if (orbit)
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationY(MathHelper.ToRadians(1f));
+                camPosition = Vector3.Transform(camPosition, rotationMatrix);
+            }
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, Vector3.Up);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            basicEffect.Projection = projectionMatrix;
+            basicEffect.View = viewMatrix;
+            basicEffect.World = worldMatrix;
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            //Turn off culling so we see both sides of our rendered triangle
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach (EffectPass pass in basicEffect.CurrentTechnique.
+                    Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawPrimitives(PrimitiveType.
+                                              TriangleList, 0, 3);
+            }
+
+            base.Draw(gameTime);
         }
     }
 }
